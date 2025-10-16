86e90f552c8731cbbe95adc1f06f09688510de59
DamianoMura <mura.damiano14@gmail.com>
2025-10-16 19:39:03 +0200

    [upd] user schema

--- app/schemas/models/Users.js
+++ app/schemas/models/Users.js
@@ -1,12 +1,13 @@
 //defining the User schema
 class User {
-  constructor(id, name) {
+  constructor(id, name, surname, email, profile_id) {
     this.id = id;
     this.name = name;
     this.surname = surname;
     this.email = email;
     this.profile_id = profile_id;
   }
+
 }
-// creating sql queries as constructor
+
 
